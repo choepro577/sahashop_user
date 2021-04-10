d1f2911d8492d539d5ad9dba5431f8dbd36eef70
bob <drhoang999@gmail.com>
2021-04-10 22:51:01 +0700

    Save

---
+++ lib/screen/inventory/products/add_product/widget/detail_select.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailSelect extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+
+      ],
+    );
+  }
+}
